2ace2b9c1a79e9e422e8892d90eded34c237caff
Utkarsh Shukla <shuklajimasthai@gmail.com>
2026-06-02 21:05:55 +0530

    containsDuplicate by hashmap

---
+++ Week_1/Day_2/containsDuplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       unordered_map<int,int>mpp;
+       for(auto it:nums)mpp[it]++;
+       for(auto it:mpp){
+        if(it.second!=1) return true;
+       }
+       return false; 
+    }
+};
