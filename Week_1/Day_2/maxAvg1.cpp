fc78e19a48e9f1354434704af3bfb692b405918f
Utkarsh Shukla <shuklajimasthai@gmail.com>
2026-06-02 18:33:00 +0530

    solved using sliding window method

---
+++ Week_1/Day_2/maxAvg1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        long long sum = 0;
+        for (int i = 0; i < k;i++) {
+            sum+=nums[i];
+        }
+        long long maxSum = sum;
+        int n = nums.size();
+        for (int i = k; i <n; i++) {
+            sum += nums[i];
+            sum -= nums[i - k];
+            maxSum = max(maxSum, sum);
+        }
+
+        return (double)maxSum / k;
+    }
+};
