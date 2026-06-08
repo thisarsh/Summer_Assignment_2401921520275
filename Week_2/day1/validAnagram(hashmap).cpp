31f7a1b45e1e1eb1b512e50a556775673b1b4845
Utkarsh Shukla <shuklajimasthai@gmail.com>
2026-06-08 22:52:46 +0530

    used hashing to solve it

---
+++ Week_2/day1/validAnagram(hashmap).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int>mpp;
+        for(auto it:s) mpp[it]++;
+
+        for(auto it :t){
+            mpp[it]--;
+        }
+        for(auto it:mpp){
+            if(it.second!=0) return false;
+        }
+        return true;
+    }
+};
