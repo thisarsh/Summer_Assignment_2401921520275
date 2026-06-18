d2c932f814cab1e844f9dc74fef7ac0d3245f967
Utkarsh Shukla <shuklajimasthai@gmail.com>
2026-06-18 14:48:41 +0530

    solved reverse polish notation

---
+++ Week_3/day4/reverse_polish_notation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int>st;
+        int i =0;
+        for(auto s:tokens){
+            if(s=="+"||s=="-"||s=="*"||s=="/"){
+                int b = st.top();
+                st.pop();
+                int a = st.top();
+                st.pop();
+                if(s=="+") st.push(a+b);
+                else if(s=="-")st.push(a-b);
+                else if(s=="*") st.push(a*b);
+                else if(s=="/") st.push(a/b);
+            }
+            else {
+                st.push(stoi(s));
+            }
+        }
+        return st.top();
+    }
+};
